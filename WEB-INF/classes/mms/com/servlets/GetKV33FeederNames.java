27ad0165994048ab082d1f94d26955c82a6723e3
ankit chourasia <ankit.chourasia@outlook.com>
2015-12-15 12:47:46 +0530

    added getKV33Details

---
+++ WEB-INF/classes/mms/com/servlets/GetKV33FeederNames.java
@@ -0,0 +1,47 @@
+package feeder.com.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import feeder.com.beans.KV33Feeder;
+import feeder.com.dao.KV33FeederDAO;
+
+public class GetKV33FeederNames extends HttpServlet{
+
+	protected void processRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+		    throws ServletException, IOException {
+		        System.out.println("Get 33kvFeederNames Called");
+		        KV33FeederDAO kv33FeederDAO=new KV33FeederDAO();
+		        ArrayList<KV33Feeder> locations=new ArrayList<KV33Feeder>();
+		        try {
+		            locations=kv33FeederDAO.getAll();
+		            System.out.println("KV 33 Feeders are : "+locations);
+		        }catch(Exception e){
+		            System.out.println("Exception in class : Get33kvFeederNames method : processRequest() : "+e);
+		        }
+		        String json = new Gson().toJson(locations);
+		        System.out.println("JSON String is : "+json);
+		        httpServletResponse.setContentType("application/json");
+		        httpServletResponse.getWriter().write(json);
+		    } 
+
+
+		    @Override
+		    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+		        processRequest(request, response);
+		    } 
+
+		    @Override
+		    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+		        processRequest(request, response);
+		    }
+}
